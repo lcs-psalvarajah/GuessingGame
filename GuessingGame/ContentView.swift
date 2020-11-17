97b1dea53c886e91089603d00d5ed1839c6fc7e5
jina <psalvarajah@lcs.on.ca>
2020-11-17 15:02:29 -0500

    Created the user interface now i have to add the code to check the numbers

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var userinput = ""
+    @State var target = Int.random(in: 1...100)
+    
+    var checkingGuess: Int {
+        
+    }
+    
+    
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack {
+                Form {
+                    Section {
+                        Text ("""
+                    I'm thinking of a number between 1 and 100.
+                    Guess what it is!
+                    """)
+                    }
+                    Section {
+                        TextField ("User Guess", text: $userinput)
+                            .keyboardType(.decimalPad)
+                    }
+                    
+                    Button("Submit Guess") {
+                        // Check the guess
+                    }
+                    
+                    Section {
+                    Text("You guessed \(userinput) ")
+                    }
+                }.navigationBarTitle("Guessing Game")
+                
+                
+            }
+            
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
